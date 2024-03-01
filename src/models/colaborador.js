6416122288c732c56f0e0b44c7534c4651a91ea4
brunogoniadis <brunogoniadislima@gmail.com>
2024-02-29 22:00:17 -0300

    feat: colection serviço criada

---
+++ src/models/colaborador.js
@@ -0,0 +1,44 @@
+/* cSpell:disable */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const colaborador = new Schema({
+  nome: {
+    type: String,
+    required: true
+  },
+  telefone: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  senha: {
+    type: String,
+    required: true
+  },
+  foto: {
+    type: String,
+    required: true
+  },
+  dataNascimento: {
+    type: String,
+    required: true
+  },
+  sexo: {
+    type: String,
+    enum:['M','F'],
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['A', 'I'],
+    required: true,
+    default: 'A'
+  }
+})
+
+
+module.exports = mongoose.model('colaborador', colaborador)
---
+++ src/models/servico.js
@@ -0,0 +1,50 @@
+/* cSpell:disable */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const servico = new Schema({
+
+    salaoId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Salao'
+    },
+    titulo: {
+        type: String,
+        require: true
+    },
+    preco: {
+        type: Number,
+        require: true
+    },
+    duracao: {
+        type: Number,
+        require: true
+    },
+    recorrencia: {
+        type: Number,
+        require: true
+    },
+    descricao: {
+        type: String,
+        require: true
+    },
+    status: {
+        type: String,
+        require: true
+    },
+    status: {
+        type: String,
+        enum: ['A', 'I', 'E'],
+        required: true,
+        default: 'A'
+    },
+
+    dataCadastro: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+servico.index({ geo: '2dsphere' })
+
+module.exports = mongoose.model('Servico', servico)
