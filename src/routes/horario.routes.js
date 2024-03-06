ec26a06ba2fbf9ea121e0528400255f9e416f3d6
brunogoniadis <brunogoniadislima@gmail.com>
2024-03-06 20:46:30 -0300

    feat: rota deletar horario

--- src/routes/horario.routes.js
+++ src/routes/horario.routes.js
@@ -12,4 +12,43 @@ router.post('/', async (req, res) => {
         res.json({ error: true, message: err.message })
     }
 })
+
+
+router.get('/salao/:salaoId', async (req, res) => {
+    try {
+        const { salaoId } = req.params;
+
+        const horarios = await Horario.find({
+            salaoId,
+        });
+
+        res.json({ error: false, horarios });
+    } catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+});
+
+router.put('/:horarioId', async (req, res) => {
+    try {
+        const { horarioId } = req.params;
+        const horario = req.body
+        await Horario.findByIdAndUpdate(horarioId, horario)
+        res.json({ error: false });
+    } catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+});
+
+router.delete(':/horarioId', async (req, res) => {
+    try {
+        const { horarioId } = req.params
+        await Horario.findByIdAndDelete(horarioId)
+        res.json({ error: false });
+    }
+    catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+})
+
+
 module.exports = router
