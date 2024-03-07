6b7a9c15d115d43274826e649c32ec404b51eb6c
brunogoniadis <brunogoniadislima@gmail.com>
2024-03-07 00:03:43 -0300

    feat: rotas de cliente criadas

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -62,4 +62,48 @@ router.post('/', async (req, res) => {
         res.json({ error: true, message: err.message });
     }
 });
+
+router.post('/filter', async (req, res) => {
+    try {
+
+        const clientes = await Cliente.find(req.body.filters);
+        res.json({ error: false, message: clientes });
+
+
+    } catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+}
+)
+
+router.get('/salao/:salaoId', async (req, res) => {
+    try {
+        const clientes = await SalaoCliente.find({
+            salaoId: req.params.salaoId,
+            status: 'A',
+        })
+            .populate('clienteId')
+            .select('clienteId');
+
+        res.json({
+            error: false,
+            clientes: clientes.map((c) => ({
+                ...c.clienteId._doc,
+                vinculoId: c._id
+            })),
+        });
+    } catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+});
+
+router.delete('/vinculo/:id', async (req, res) => {
+    try {
+        await SalaoCliente.findByIdAndUpdate(req.params.id, { status: 'I' });
+        res.json({ error: false });
+    } catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+});
+
 module.exports = router
