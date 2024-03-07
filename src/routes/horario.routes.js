505b2df83a3c448104b6b0227dfc8d909970ae6c
brunogoniadis <brunogoniadislima@gmail.com>
2024-03-06 21:06:05 -0300

    feat: rota de get colaborador

--- src/routes/horario.routes.js
+++ src/routes/horario.routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const horario = require('../models/horario');
 const Horario = require('../models/horario');
-
+const ColaboradorServico = require('../models/relationship/colaboradorSevico')
 router.post('/', async (req, res) => {
     try {
         const horario = await new Horario(req.body).save()
@@ -38,6 +38,24 @@ router.put('/:horarioId', async (req, res) => {
         res.json({ error: true, message: err.message });
     }
 });
+
+router.post('/colaboradores', async (req, res) => {
+    try {
+        const colaboradorServico = await ColaboradorServico.find({
+            servicoId: { $in: req.body.especialidades },
+            status: 'A'
+        }).populate('colaboradorId', 'nome').select('colaboradorId -_id');
+
+        const listaColaboradores = colaboradorServico.map(vinculo => ({
+            label: vinculo.colaboradorId.nome,
+            value: vinculo.colaboradorId._id
+        }));
+
+        res.json({ error: false, listaColaboradores })
+    } catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+})
 
 router.delete(':/horarioId', async (req, res) => {
     try {
